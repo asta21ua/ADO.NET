d6d076e91f8d6530acdb63f4062ea29471d64636
asta21ua <asta21ua@yahoo.com>
2019-02-20 01:34:48 +0400

    Connecton type USING was added

---
+++ ADO.NET database set/ADO.NET database connection Using/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace ADO.NET_database_connection_Using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // connecting local DataBase with USING to close Automatically 
+
+            String connectionString = @"Data Source = .\SQLEXPRESS; Initial Catalog = userdb; Integrated Source= True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                Console.WriteLine("Connecton is Open");
+            }
+            Console.WriteLine("Connection is Closed");
+
+            Console.ReadLine(); 
+        }
+    }
+}
